6d05ef0313d03251665976fa8d143c154f07b195
satish <satishsharma.cse@gmail.com>
2020-08-09 13:51:29 +0530

    added put path delete post

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
+
+
 
 
 class HelloApiView(APIView):
     """test api view"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Return a list of apiview"""
@@ -15,3 +23,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'hello','an_apiview':an_apiview})
+
+
+    def post(self,request):
+        """create a hello message with our name"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(
+            serializer.errors,status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self,request,pk=None):
+        """handle updating an object"""
+        return Response({'method':'PUT'})
+
+
+    def patch(self,request,pk=None):
+        """handle partial update of an object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """delete an object"""
+        return Response({'method':'DELETE'})
